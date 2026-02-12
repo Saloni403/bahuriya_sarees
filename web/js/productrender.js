f314960cf2d806071acffaef7fa7760085bf022a
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2026-02-12 16:31:22 +0530

    done

--- web/js/productrender.js
+++
@@ -1,6 +0,0 @@
-const prod_card = document.querySelectorAll(".prod_card")
-prod_card.forEach(card=>{
-    card.addEventListener("click", ()=>{
-        location.href='product-description.html';
-    })
-})
